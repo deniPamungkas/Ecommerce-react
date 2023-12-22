75aa2c8b0fe948234974b04b2e9856a5c19349d6
deniPamungkas <pamungkasdeni18@gmail.com>
2023-12-22 22:02:42 +0700

    kurang mobile

--- src/components/pages/cart.jsx
+++ src/components/pages/cart.jsx
@@ -1,5 +1,7 @@
 import bg from "../../assets/images/bgShop1.jpg";
 import OfferBar from "../molecules/offerBar";
+import trash from "../../assets/icons/trash.png";
+import Button from "../atoms/button";
 
 const Cart = () => {
   return (
@@ -15,6 +17,64 @@ const Cart = () => {
             Cart
           </h1>
           <p className="text-center font-semibold z-20 text-white">{`home > cart`}</p>
+        </div>
+      </div>
+      <div className="w-full h-fit block px-3 md:px-7 lg:flex lg:gap-x-5 xl:gap-x-10 lg:px-10 xl:px-16 md:py-7 xl:py-14">
+        <div className="lg:w-3/4 xl:w-2/3 h-full mb-10 lg:mb-0">
+          <table className="table-auto w-full">
+            <thead className="">
+              <tr className="h-[50px] bg-filter text-start">
+                <th></th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-start">
+                <td>
+                  <div className="w-20 h-20 bg-teal-200 rounded-lg"></div>
+                </td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+                <td>Alfreds Futterkiste</td>
+                <td>Germany</td>
+                <td>
+                  <img src={trash} alt="jhjhjhjh" className="w-5 h-5" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="lg:w-1/4 xl:w-1/3 h-fit bg-filter md:py-10 py-5 px-10 md:px-32 lg:px-8 xl:px-16">
+          <h1 className="font-bold lg:text-xl xl:text-2xl text-center mb-10">
+            Cart Totals
+          </h1>
+          <div className="flex w-full justify-between items-center mb-7">
+            <span className="font-semibold lg:text-base xl:text-lg">
+              Subtotal:
+            </span>
+            <span className="font-semibold lg:text-sm xl:text-base text-gray-700">
+              Rp. 2.500.000
+            </span>
+          </div>
+          <div className="flex w-full justify-between mb-10">
+            <span className="font-semibold lg:text-base xl:text-lg">
+              Total:
+            </span>
+            <span className="font-semibold text-lg md:text-xl lg:text-base xl:text-xl text-primary">
+              Rp. 2.500.000
+            </span>
+          </div>
+          <Button
+            className={
+              "border-2 border-black h-[40px] w-[100px] text-base md:h-[40px] md:w-[120px] xl:h-[50px] xl:w-[150px] rounded-xl m-auto md:text-lg xl:text-xl font-semibold"
+            }
+          >
+            Checkout
+          </Button>
         </div>
       </div>
       <OfferBar />
